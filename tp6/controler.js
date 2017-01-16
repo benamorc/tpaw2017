47086653ff1979a2f9007ae19e7c59401fb108af
benamorc <chaouki.ben.amor@hotmail.com>
2017-01-16 11:27:54 +0100

    Update controler.js

--- tp6/controler.js
+++ tp6/controler.js
@@ -5,11 +5,6 @@ $scope.recherche = function() {
 /* appel AJAX à l’API openweathermap */
 $http.get('https://demo.bilelz.fr/owmap/?q='+$scope.city+'&units=metric&appid=3cc8d6964f710f7694ebbc7ffb7bf6bd)
 .success(function(data) {
-[
-{
-}
-]
-
 $scope.meteo = data;
 }).error(function(data) {
 /* en cas d’erreur */
@@ -17,52 +12,3 @@ $scope.errorMsg = "Hum. Error... please retry.";
 });
 }
 }]);
-
-function gps(){
-
-   if (navigator.geolocation) {
-
-       navigator.geolocation.getCurrentPosition(showPosition, showError);
-
-   } else {
-
-       document.getElementById("nom").innerHTML = "Geolocation is not supported by this browser.";
-
-   }
-
-}
-function showPosition(position) {
-	  searchLatLng(position.coords.latitude , position.coords.longitude)
-
-}
-function showError(error) {
-
- var x = document.getElementById("nom");
-
-   switch(error.code) {
-
-       case error.PERMISSION_DENIED:
-
-           x.innerHTML = "User denied the request for Geolocation."
-
-           break;
-
-       case error.POSITION_UNAVAILABLE:
-
-           x.innerHTML = "Location information is unavailable."
-
-           break;
-
-       case error.TIMEOUT:
-
-           x.innerHTML = "The request to get user location timed out."
-
-           break;
-
-       case error.UNKNOWN_ERROR:
-
-           x.innerHTML = "An unknown error occurred."
-
-           break;
-
-   }}
