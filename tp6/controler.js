3d3293d97069dade70ab17b132c2f23873d9fa90
benamorc <chaouki.ben.amor@hotmail.com>
2017-01-16 11:54:51 +0100

    Update controler.js

--- tp6/controler.js
+++ tp6/controler.js
@@ -3,7 +3,7 @@ meteoControllers.controller('MainController', ['$scope', '$http',
 function($scope, $http) {
 $scope.recherche = function() {
 /* appel AJAX à l’API openweathermap */
-$http.get('https://demo.bilelz.fr/owmap/?q='+$scope.city+'&units=metric&appid=3cc8d6964f710f7694ebbc7ffb7bf6bd)
+$http.get('https://demo.bilelz.fr/owmap/?q='+$scope.city+'&units=metric&appid=3cc8d6964f710f7694ebbc7ffb7bf6bd')
 .success(function(data) {
 $scope.meteo = data;
 }).error(function(data) {
